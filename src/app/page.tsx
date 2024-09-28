a30a5ba3aab7dbfebbbda22bb261d33da58b8bd3
JT Turner <jtwebman@gmail.com>
2024-09-27 20:32:18 -0700

    Cleanu not needed mock data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,6 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls: string[] = Array(40).fill(
-  "https://picsum.photos/200",
-) as string[];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: (index + 1).toString(),
-  url: url.replace("/200", `/id/${index + 1}/300/200`),
-}));
-
 export default async function HomePage() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
